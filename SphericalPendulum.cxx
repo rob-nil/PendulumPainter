760cd1b0a0d506ef81a082fd9aca630279e415b3
rob-nil <holzer_patrick@gmx.at>
2022-02-06 13:15:08 +0100

    Winkel Bug Seile Solved

--- SphericalPendulum.cxx
+++ SphericalPendulum.cxx
@@ -137,7 +137,7 @@ matrix SphericalPendulum::getMatVTK(matrix& matX) {
 		fVec[0] = (r + l) * cos(matX[i][2]);
 		fVec[1] = (r + l) * sin(matX[i][2]);
 		fVec[2] = (matX[i + 1][0] - matX[i][0]) / degToRad;
-		fVec[3] = matX[i + 1][2] - matX[i][2] / degToRad;
+		fVec[3] = (matX[i + 1][2] - matX[i][2]) / degToRad;
 		fMat.push_back(fVec);
 	}
 	cout << "\n>>>> Values for GUI! ------------------------------------------------------\n\n";
